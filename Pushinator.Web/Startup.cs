8cd19df22f93416723709fbfc828f946d93b2b24
Kirill Levchenko <k.levchenko@dodopizza.com>
2020-12-13 16:30:17 +0300

    Add spa

--- Pushinator.Web/Startup.cs
+++ Pushinator.Web/Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,6 +13,8 @@ namespace Pushinator.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "Client/public"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,7 +32,17 @@ namespace Pushinator.Web
                 endpoints.MapControllerRoute(
                     "default",
                     "{controller=Home}/{action=Index}/{id?}");
-            });;
+            });
+            
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "client";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer("start");
+                }
+            });
         }
     }
 }
