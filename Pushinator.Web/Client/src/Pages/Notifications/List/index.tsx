326b54d7e46c5553494361be77e603f57dd0f5e0
Kirill Levchenko <k.levchenko@dodopizza.com>
2021-04-13 06:25:28 +0300

    fix frontend

--- Pushinator.Web/Client/src/Pages/Notifications/List/index.tsx
+++ Pushinator.Web/Client/src/Pages/Notifications/List/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Toolbar } from '../../../Components/Toolbar'
 import { Button, Container, createStyles, Theme } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
@@ -13,43 +13,53 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const NotificationList: React.FC<any> = () => {
 	const classes = useStyles()
-	const [isPushButtonVisible, setIsPushButtonVisible] = useState(false)
 
+	return (
+		<>
+			<Toolbar />
+			<Container className={classes.clientPushButton}>
+				<Button>Click me</Button>
+			</Container>
+		</>
+	)
+
+	// const [isPushButtonVisible, setIsPushButtonVisible] = useState(false)
+	//
 	// useEffect(() => {
 	// 	navigator.serviceWorker.register('/serviceWorker.js').then((x) => {
 	// 		setIsPushButtonVisible(true);
 	// 	})
 	// 	Notification.requestPermission().then((s) => console.log('notification status', s))
 	// }, [])
-
-	const showNotification = () => {
-		navigator.serviceWorker.getRegistration().then((r) => r?.showNotification('Test local notification'))
-	}
-
-	const longCalculation = () => {
-		navigator.serviceWorker.getRegistration().then((r) => {
-			r?.active?.postMessage({ max: 199999 })
-		})
-	}
-
-	return (
-		<>
-			<Toolbar />
-
-			<Container className={classes.clientPushButton}>
-				{isPushButtonVisible && (
-					<>
-						<Button variant="contained" onClick={showNotification}>
-							Клиентский пуш
-						</Button>
-						<Button variant="contained" onClick={longCalculation}>
-							Долгие вычисление и показа пуша
-						</Button>
-					</>
-				)}
-			</Container>
-		</>
-	)
+	//
+	// const showNotification = () => {
+	// 	navigator.serviceWorker.getRegistration().then((r) => r?.showNotification('Test local notification'))
+	// }
+	//
+	// const longCalculation = () => {
+	// 	navigator.serviceWorker.getRegistration().then((r) => {
+	// 		r?.active?.postMessage({ max: 199999 })
+	// 	})
+	// }
+	//
+	// return (
+	// 	<>
+	// 		<Toolbar />
+	//
+	// 		<Container className={classes.clientPushButton}>
+	// 			{isPushButtonVisible && (
+	// 				<>
+	// 					<Button variant="contained" onClick={showNotification}>
+	// 						Клиентский пуш
+	// 					</Button>
+	// 					<Button variant="contained" onClick={longCalculation}>
+	// 						Долгие вычисление и показа пуша
+	// 					</Button>
+	// 				</>
+	// 			)}
+	// 		</Container>
+	// 	</>
+	// )
 }
 
 export default NotificationList
